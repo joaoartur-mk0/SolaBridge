2810d1c5e34f98e280af0d2b092b71a714935c88
joaoartur-mk0 <joaoartur655@gmail.com>
2026-06-28 00:56:23 -0300

    feat: Adicinado controller do modulo de venda

---
+++ backend/app/Http/Controllers/VendaController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVendaRequest;
+use App\Services\VendaService;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+
+class VendaController extends Controller
+{
+    use ApiResponse;
+
+    protected $vendaService;
+
+    public function __construct(VendaService $vendaService)
+    {
+        $this->vendaService = $vendaService;
+    }
+
+    public function store(StoreVendaRequest $request): JsonResponse
+    {
+        $dadosLimpos = $request->validated();
+        $tenant_id = 1;
+
+        $resultado = $this->vendaService->processarVenda(
+            $dadosLimpos,
+            $tenant_id,
+        );
+
+        return $this->succesResponse(
+            "Venda processada com sucesso!",
+            $resultado,
+            201,
+        );
+    }
+}
